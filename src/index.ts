db69830bcba97a983da987d5583a8d0eed298f3f
Galitan-dev <galitan.dev@gmail.com>
2022-02-23 11:47:18 +0100

    chore: Added Prettier, updated architecture, and wrote a sample command

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import chalk from 'chalk';
-import { log } from 'console';
-import figlet from 'figlet';
-
-log(chalk.bgCyan.red.bold(figlet.textSync('Hey guys !')));
---
+++ src/main/index.ts
@@ -0,0 +1,34 @@
+import chalk from 'chalk';
+import path from 'path';
+import argvSync from './parseArgs';
+
+(async () => {
+	const argv = await argvSync;
+
+	let command;
+	const trace = [__dirname, 'commands'];
+	const args: (string | number)[] = [];
+	for (const arg of argv._) {
+		if (command) {
+			args.push(arg);
+		}
+
+		trace.push(arg.toString());
+		try {
+			command = await import(path.resolve(...trace));
+		} catch {
+			continue;
+		}
+	}
+
+	if (!command) {
+		return console.error(
+			chalk.red('Unknown command') +
+				', please refer to the help page ' +
+				chalk.grey('(gabum --help)') +
+				'.'
+		);
+	}
+
+	command.default(argv, args);
+})();
