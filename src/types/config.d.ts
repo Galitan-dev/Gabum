8f63de96e9a2ca043d00fa5f1f4ca6e67d0c7e06
Galitan-dev <galitan.dev@gmail.com>
2022-03-08 20:37:03 +0100

    style: edited rules

--- src/types/config.d.ts
+++ src/types/config.d.ts
@@ -1,11 +1,11 @@
 declare interface DefaultProjectSettings {
-  private: boolean;
-  type: string;
-  language: string;
-  license: string;
+    private: boolean;
+    type: string;
+    language: string;
+    license: string;
 }
 
 declare interface Configuration {
-  directory: string;
-  defaultProjectSettings: DefaultProjectSettings;
+    directory: string;
+    defaultProjectSettings: DefaultProjectSettings;
 }
--- src/types/prompts.d.ts
+++ src/types/prompts.d.ts
@@ -17,84 +17,84 @@ import { Chalk } from 'chalk';
 import { Readable, Writable } from 'node:stream';
 
 declare namespace prompts {
-  // Based upon: https://github.com/terkelg/prompts/blob/d7d2c37a0009e3235b2e88a7d5cdbb114ac271b2/lib/elements/select.js#L29
-  interface Choice {
-    title: string;
-    value?: Value;
-    disabled?: boolean | undefined;
-    selected?: boolean | undefined;
-    description?: string | undefined;
-  }
+    // Based upon: https://github.com/terkelg/prompts/blob/d7d2c37a0009e3235b2e88a7d5cdbb114ac271b2/lib/elements/select.js#L29
+    interface Choice {
+        title: string;
+        value?: Value;
+        disabled?: boolean | undefined;
+        selected?: boolean | undefined;
+        description?: string | undefined;
+    }
 
-  interface Options<T> {
-    onSubmit?: ((prompt: PromptObject, answer: T, answers: T[]) => void) | undefined;
-    onCancel?: ((prompt: PromptObject, answers: T) => void) | undefined;
-  }
+    interface Options<T> {
+        onSubmit?: ((prompt: PromptObject, answer: T, answers: T[]) => void) | undefined;
+        onCancel?: ((prompt: PromptObject, answers: T) => void) | undefined;
+    }
 
-  interface PromptObject<T extends string = string> {
-    type: PromptType | Falsy | PrevCaller<T, PromptType | Falsy>;
-    name: ValueOrFunc<T>;
-    message?: ValueOrFunc<string> | undefined;
-    initial?:
-      | InitialReturnValue
-      | PrevCaller<T, InitialReturnValue | Promise<InitialReturnValue>>
-      | undefined;
-    style?: string | PrevCaller<T, string | Falsy> | undefined;
-    format?: PrevCaller<T, void> | undefined;
-    validate?: PrevCaller<T, boolean | string | Promise<boolean | string>> | undefined;
-    onState?: (color: Chalk) => void | undefined;
-    onRender?: (color: Chalk) => void | undefined;
-    min?: number | PrevCaller<T, number | Falsy> | undefined;
-    max?: number | PrevCaller<T, number | Falsy> | undefined;
-    float?: boolean | PrevCaller<T, boolean | Falsy> | undefined;
-    round?: number | PrevCaller<T, number | Falsy> | undefined;
-    instructions?: string | boolean | undefined;
-    increment?: number | PrevCaller<T, number | Falsy> | undefined;
-    separator?: string | PrevCaller<T, string | Falsy> | undefined;
-    active?: string | PrevCaller<T, string | Falsy> | undefined;
-    inactive?: string | PrevCaller<T, string | Falsy> | undefined;
-    choices?: Choice[] | PrevCaller<T, Choice[] | Falsy> | undefined;
-    hint?: string | PrevCaller<T, string | Falsy> | undefined;
-    warn?: string | PrevCaller<T, string | Falsy> | undefined;
-    suggest?: ((input: T, choices: Choice[]) => Promise<T>) | undefined;
-    limit?: number | PrevCaller<T, number | Falsy> | undefined;
-    mask?: string | PrevCaller<T, string | Falsy> | undefined;
-    stdout?: Writable | undefined;
-    stdin?: Readable | undefined;
-    msg?: string;
-    _value?: T;
-    rendered?: string;
-  }
+    interface PromptObject<T extends string = string> {
+        type: PromptType | Falsy | PrevCaller<T, PromptType | Falsy>;
+        name: ValueOrFunc<T>;
+        message?: ValueOrFunc<string> | undefined;
+        initial?:
+            | InitialReturnValue
+            | PrevCaller<T, InitialReturnValue | Promise<InitialReturnValue>>
+            | undefined;
+        style?: string | PrevCaller<T, string | Falsy> | undefined;
+        format?: PrevCaller<T, void> | undefined;
+        validate?: PrevCaller<T, boolean | string | Promise<boolean | string>> | undefined;
+        onState?: (color: Chalk) => void | undefined;
+        onRender?: (color: Chalk) => void | undefined;
+        min?: number | PrevCaller<T, number | Falsy> | undefined;
+        max?: number | PrevCaller<T, number | Falsy> | undefined;
+        float?: boolean | PrevCaller<T, boolean | Falsy> | undefined;
+        round?: number | PrevCaller<T, number | Falsy> | undefined;
+        instructions?: string | boolean | undefined;
+        increment?: number | PrevCaller<T, number | Falsy> | undefined;
+        separator?: string | PrevCaller<T, string | Falsy> | undefined;
+        active?: string | PrevCaller<T, string | Falsy> | undefined;
+        inactive?: string | PrevCaller<T, string | Falsy> | undefined;
+        choices?: Choice[] | PrevCaller<T, Choice[] | Falsy> | undefined;
+        hint?: string | PrevCaller<T, string | Falsy> | undefined;
+        warn?: string | PrevCaller<T, string | Falsy> | undefined;
+        suggest?: ((input: T, choices: Choice[]) => Promise<T>) | undefined;
+        limit?: number | PrevCaller<T, number | Falsy> | undefined;
+        mask?: string | PrevCaller<T, string | Falsy> | undefined;
+        stdout?: Writable | undefined;
+        stdin?: Readable | undefined;
+        msg?: string;
+        _value?: T;
+        rendered?: string;
+    }
 
-  type Answers<T extends string> = {
-    [id in T]: Value;
-  };
+    type Answers<T extends string> = {
+        [id in T]: Value;
+    };
 
-  type PrevCaller<T extends string, R = T> = (
-    prev: unknown,
-    values: Answers<T>,
-    prompt: PromptObject
-  ) => R;
+    type PrevCaller<T extends string, R = T> = (
+        prev: unknown,
+        values: Answers<T>,
+        prompt: PromptObject
+    ) => R;
 
-  type Falsy = false | null | undefined;
+    type Falsy = false | null | undefined;
 
-  type PromptType =
-    | 'text'
-    | 'password'
-    | 'invisible'
-    | 'number'
-    | 'confirm'
-    | 'list'
-    | 'toggle'
-    | 'select'
-    | 'multiselect'
-    | 'autocomplete'
-    | 'date'
-    | 'autocompleteMultiselect';
+    type PromptType =
+        | 'text'
+        | 'password'
+        | 'invisible'
+        | 'number'
+        | 'confirm'
+        | 'list'
+        | 'toggle'
+        | 'select'
+        | 'multiselect'
+        | 'autocomplete'
+        | 'date'
+        | 'autocompleteMultiselect';
 
-  type Value = string | number | object | boolean | Date;
+    type Value = string | number | object | boolean | Date;
 
-  type ValueOrFunc<T extends string> = T | PrevCaller<T>;
+    type ValueOrFunc<T extends string> = T | PrevCaller<T>;
 
-  type InitialReturnValue = string | number | boolean | Date;
+    type InitialReturnValue = string | number | boolean | Date;
 }
